b75013e7f377fbe0f5f695719a7283a202998550
Logan Senjov <logansenjov@gmail.com>
2017-07-29 01:49:38 +1000

    Basic functionality is working

--- src/cljs/diligence/core.cljs
+++ src/cljs/diligence/core.cljs
@@ -6,8 +6,90 @@
             [goog.history.EventType :as HistoryEventType]
             [markdown.core :refer [md->html]]
             [diligence.ajax :refer [load-interceptors!]]
-            [ajax.core :refer [GET POST]])
+            [ajax.core :refer [GET POST] :as ajax])
   (:import goog.History))
+
+;; People is a vector of people
+(defonce app-state (r/atom {:people {}
+                            :selected 1
+                            :last-selection 0
+                            }))
+
+(defn get-data-on-person
+  [id {:keys [first-name last-name website] :as request}]
+  (println "Getting data on person. Id: " id " request: " (pr-str request))
+  (GET "/api/edn/get-banned"
+       {:params request
+        ;:response-format (ajax/transit-response-format)
+        :handler #(let [ret (cljs.reader/read-string %)]
+                    (println "Query return: " (pr-str ))
+                    (swap! app-state assoc-in [:people id :banned-person] ret))
+        }))
+
+(defn search-component
+  []
+  (let [query-atom (r/atom {})
+        item-component (fn [kw s]
+                         ^{:key kw}
+                         [:div
+                          [:label {:class "control-label"} s]
+                          [:input {:class "form-control"
+                                   :type "text"
+                                   :on-change #(swap! query-atom assoc kw (-> % .-target .-value))
+                                   :value (get @query-atom kw)
+                                   :label s}]])]
+    (fn []
+      [:div.col-lg-4
+       [:form.form-horizontal
+        (doall (map item-component
+                    [:first-name :last-name :website]
+                    ["First name" "Last name" "Website"]))]
+       [:div.btn.btn-default
+        {:onClick #(let [request @query-atom]
+                     (reset! query-atom {})
+                     (->
+                       (swap! app-state
+                              (comp
+                                (fn [a] (assoc-in a [:people (:last-selection a)] request))
+                                (fn [a] (update-in a [:last-selection] inc))))
+                       :last-selection
+                       (get-data-on-person request)))}
+        "Search"]
+       ])))
+
+(defn banned-person-component
+  []
+  (if-let [res (get-in @app-state [:people (:selected @app-state) :banned-person])]
+    ;; A record was returned
+    [:div.col-lg-4
+     [:div.panel.panel-default
+      [:div.panel-heading "ASIC Banned List:"]
+      [:div.panel-body
+       (if (pos? (count res))
+         [:table.table.table-striped.table-hover
+          [:thead>tr
+           [:th "Record Name"]
+           [:th "Start Date"]
+           [:th "End Date"]
+           [:th "Ban Type"]
+           [:th "Area"]
+           ]
+          [:tbody
+          (doall (map (fn [id {:keys [bd_per_end_dt, bd_per_start_dt, bd_per_name, bd_per_add_country, bd_per_add_state, bd_per_add_local, bd_per_type]}]
+                        ^{:key id}
+                        [:tr
+                         [:td bd_per_name]
+                         [:td bd_per_start_dt]
+                         [:td bd_per_end_dt]
+                         [:td bd_per_type]
+                         [:td (str bd_per_add_local ", " bd_per_add_state ", " bd_per_add_country)]
+                         ])
+                      (range)
+                      (sort-by :bd_per_name res)))]])
+       ]
+      ]
+     ]
+    ))
 
 (defn nav-link [uri title page collapsed?]
   [:li.nav-item
@@ -38,9 +120,22 @@
 (defn home-page []
   [:div.container
    (when-let [docs (session/get :docs)]
-     [:div.row>div.col-sm-12
-      [:div {:dangerouslySetInnerHTML
-             {:__html (md->html docs)}}]])])
+     [:div.row>div.col-lg-12
+   [:div.btn-toolbar
+    [:div.btn-group
+     (doall
+       (map (fn [[id {:keys [first-name last-name]}]]
+              ^{:key id}
+              [:div {:class (if (= (:selected @app-state) id) "btn btn-primary" "btn btn-default")
+                     :onClick #(swap! app-state assoc :selected id)
+                     }
+               (str last-name ", " first-name)])
+            (:people @app-state)))
+     ]
+    ]
+   [search-component]
+   [banned-person-component]
+   (pr-str @app-state)])])
 
 (def pages
   {:home #'home-page
